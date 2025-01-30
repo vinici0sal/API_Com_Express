6cb038fe53aa5dbdf9d218e9c7c9edb235a6fee9
Vinicius Loreto <viniciusgarcialoreto@gmail.com>
2025-01-30 11:51:36 -0300

    new possibility

--- Portifolio/JS/new.js
+++ Portifolio/JS/new.js
@@ -1,21 +1,46 @@
 import exp from 'express'
-import { readBuilderProgram } from 'typescript';
+import read from 'readline-sync'
 
 const app = exp();
 var data = new Date();
 
-app.get('/', (req, res) => {
-    var altura = req.query.altura;
-    var peso = req.query.peso;
+let resposta = read.question("Gostaria de iniciar quais das apis? ")
 
-    let imc = peso / (altura * altura);
+if(resposta == "1"){
 
-    res.json({imc : imc});
-})
+    app.get('/', (req, res) => {
+    
+        var altura = req.query.altura;
+        var peso = req.query.peso;
+    
+        let imc = peso / (altura * altura);
+    
+        console.log({imc : imc});
+    })
+    
+    app.listen(8080, () => {
+        let min = data.getMinutes()
+        let date = data.getHours() + ':' + min; 
+        console.log(`Servidor conectado as ${date} da manhã` )
+        }
+    )
+}
 
-app.listen(8080, () => {
-    let min = data.getMinutes()
-    let date = data.getHours() + ':' + min; 
-    console.log(`Servidor conectado as ${date} da manhã` )
-    }
-)
+else if(resposta == "2"){
+    app.get('/', (req, res) => {
+    
+        var altura = req.query.altura;
+        var peso = req.query.peso;
+    
+        let imc = peso / (altura * altura);
+    
+        res.json({imc : imc})
+    })
+    
+    app.listen(8080, () => {
+        let min = data.getMinutes()
+        let date = data.getHours() + ':' + min; 
+        console.log(`Servidor conectado as ${date} da manhã` )
+        }
+    )
+}
